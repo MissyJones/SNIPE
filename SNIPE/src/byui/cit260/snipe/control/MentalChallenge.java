f8fab685b10cb377e71320d8c37a3328c739ba35
Home <Home@192.168.1.69>
2017-02-08 16:17:43 -0600

    1/26/2017 - Initial commit, Missy

---
+++ SNIPE/src/byui/cit260/snipe/control/MentalChallenge.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.snipe.control;
+import java.io.Serializable;
+import java.util.Objects;
+import byui.cit260.snipe.model.Game;
+/**
+ *
+ * @author Home
+ */
+public class MentalChallenge{
+public String mathPuzzleThree(double height, double width, double answer){
+ double area= height*width;
+ String response;
+if (area <1){
+response="validation error";
+}
+else if (area != answer){
+response= "No script for you!";}
+
+else { 
+response="CONGRATS!";
+}
+return response;
+}
+}
+
+    
