794091f2b18d202af3c7a6555efaf87f1f4e63c7
BuildTools <unconfigured@null.spigotmc.org>
2017-02-25 11:48:14 -0600

    Changes!!!

---
+++ SNIPE/test/byui/cit260/snipe/control/GameControlTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.snipe.control;
+
+import byui.cit260.snipe.model.Player;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Maxwell
+ */
+public class GameControlTest {
+    
+    public GameControlTest() {
+    }
+
+    /**
+     * Test of createPlayer method, of class GameControl.
+   
+    @Test
+    public void testCreatePlayer() {
+        System.out.println("createPlayer");
+        String playersName = "";
+        Player expResult = null;
+        Player result = GameControl.createPlayer(playersName);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+
+     * Test of createNewGame method, of class GameControl.
+     
+    @Test
+    public void testCreateNewGame() {
+        System.out.println("createNewGame");
+        Player player = null;
+        GameControl.createNewGame(player);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+*/
+    /**
+     * Test of codeNameGen method, of class GameControl.
+     */
+    @Test
+    public void testCodeNameGen() {
+        System.out.println("codeNameGen");
+        String result = GameControl.codeNameGen();
+        System.out.println(result);
+        if (result == "Broken Code") {
+                   fail("The test case is a prototype."); 
+        }
+        else {
+            System.out.println("The code works");
+        }
+
+    }
+    
+}
---
+++ SNIPE/test/byui/cit260/snipe/control/PhysicalChallengeControlTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.snipe.control;
+
+import byui.cit260.snipe.model.PhysChallenge;
+import byui.cit260.snipe.model.Player;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Maxwell
+ */
+public class PhysicalChallengeControlTest {
+    
+    public PhysicalChallengeControlTest() {
+    }
+
+    /**
+     * Test of dmgCalc method, of class PhysicalChallengeControl.
+     */
+    @Test
+    public void testDmgCalc() {
+        System.out.println("dmgCalc");
+        PhysChallenge physChallenge = null;
+        Player player = null;
+        PhysicalChallengeControl instance = new PhysicalChallengeControl();
+        String expResult = "";
+        String result = instance.dmgCalc(physChallenge, player);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+    
+}
