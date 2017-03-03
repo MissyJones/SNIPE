3b83df607c26d8aaeb5824245dc6d505b4a85607
Home <Home@192.168.1.69>
2017-03-03 11:15:17 -0600

    1/26/2017 - Initial commit, Missy

---
+++ SNIPE/src/byui/cit260/snipe/view/GameOverLoseView.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.snipe.view;
+
+/**
+ *
+ * @author Home
+ */
+public class GameOverLoseView {
+private final String displayMessage;
+
+public GameOverLoseView(){
+    this.displayMessage = "\nEPIC Mission FAIL!Go back to the Main Menu and try again.";
+}
+                          
+      private void MainMenuView(){
+                System.out.print("Back to the Main Menu.");
+    }                     
+    
+}   
