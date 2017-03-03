32ddd31603202c2206822bd55a457c058497ae1e
Home <Home@192.168.1.69>
2017-03-03 11:15:58 -0600

    1/26/2017 - Initial commit, Missy

---
+++ SNIPE/src/byui/cit260/snipe/view/GameOverWinView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.snipe.view;
+/**
+ *
+ * @author Home
+ */
+public class GameOverWinView {
+
+private final String displayMessage;
+
+public GameOverWinView(){
+    this.displayMessage = "\nCONGRATULATIONS! YOU WON! Welcome to the elite S.N.I.P.E.";
+}
+                          
+      private void MainMenuView(){
+                System.out.print("Back to the Main Menu.");
+    }                     
+    
+}
+
