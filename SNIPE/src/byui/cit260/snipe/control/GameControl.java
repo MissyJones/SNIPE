8385084c80e6acfa705af0828763ba867c1954fa
Home <Home@192.168.1.69>
2017-03-10 14:02:52 -0600

    1/26/2017 - Initial commit, Missy

--- SNIPE/src/byui/cit260/snipe/control/GameControl.java
+++ SNIPE/src/byui/cit260/snipe/control/GameControl.java
@@ -5,7 +5,9 @@
  */
 package byui.cit260.snipe.control;
 
+import byui.cit260.snipe.model.Game;
 import byui.cit260.snipe.model.Player;
+import java.util.Map;
 import snipe.SNIPE;
 
 /**
@@ -14,35 +16,39 @@ import snipe.SNIPE;
  */
 public class GameControl {
 
-
-    
-    
     public static Player createPlayer(String playersName) {
         if (playersName == null) {
             return null;
         }
-        
+
         Player player = new Player();
         player.setName(playersName);
         player.setCodeName(codeNameGen());
-        
-                System.out.println("\n"+playersName+"! Your codename is: "+player.getCodeName()+". "
-                + "\n\n Go get the bad guys, "+player.getCodeName()+"!");
-        
+
+        System.out.println("\n" + playersName + "! Your codename is: " + player.getCodeName() + ". "
+                + "\n\n Go get the bad guys, " + player.getCodeName() + "!");
+
         SNIPE.setPlayer(player);
         return player;
     }
 
     public static void createNewGame(Player player) {
-        System.out.println("called GameControl.createNewGame*******");
+       Game game = new Game();
+       SNIPE.setCurrentGame(game);
+       
+       game.setPlayer(player);
+       
+       Map map = MapControl.createMap();
+       game.setMap(map);
     }
-public static String codeNameGen () {
-        int rand1 = (int) Math.ceil(Math.random()*5);
-        int rand2 = (int) Math.ceil(Math.random()*5);
-        
+
+    public static String codeNameGen() {
+        int rand1 = (int) Math.ceil(Math.random() * 5);
+        int rand2 = (int) Math.ceil(Math.random() * 5);
+
         String code1;
         String code2;
-        
+
         switch (rand1) {
             case 0:
                 code1 = "Silent ";
@@ -89,9 +95,8 @@ public static String codeNameGen () {
                 code2 = "Code";
                 break;
         }
-        
-        String codeName = code1+code2;
 
+        String codeName = code1 + code2;
 
         return codeName;
 
