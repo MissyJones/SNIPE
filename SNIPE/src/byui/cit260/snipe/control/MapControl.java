46e61e48715deba430df50e0d96ef848c09e26d1
Home <Home@192.168.1.69>
2017-03-10 14:16:09 -0600

    1/26/2017 - Initial commit, Missy

--- SNIPE/src/byui/cit260/snipe/control/MapControl.java
+++ SNIPE/src/byui/cit260/snipe/control/MapControl.java
@@ -7,14 +7,27 @@ package byui.cit260.snipe.control;
 
 import byui.cit260.snipe.model.Location;
 import java.io.Serializable;
+import java.util.Map;
 
 /**
  *
  * @author Maxwell
  */
 public class MapControl implements Serializable {
+
+    /**
+     *
+     * @return
+     */
     private int noOfRows;
-    private int noOfColumns;
+    private int noOfComlumns;
     private Location[][] locations;
+    
+    public static Map createMap(){
+     Map map = null;
+     
+     System.out.println("\n***createMap() called***");
+     return map;
+   }
 
 }
