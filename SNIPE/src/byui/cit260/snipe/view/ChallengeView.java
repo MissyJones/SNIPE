4d644a511107486ab052bdb05f9d120962bfbfe2
elizabethkirby <elizabethkirby@elizabeths-mbp.fios-router.home>
2017-03-29 20:08:09 -0400

    Added scene descriptions. still need to get Amazon Rain Forest (which is already written) in, and have to write up the others for Brazil, Australia, and Russia.

--- SNIPE/src/byui/cit260/snipe/view/ChallengeView.java
+++ SNIPE/src/byui/cit260/snipe/view/ChallengeView.java
@@ -5,18 +5,23 @@
  */
 package byui.cit260.snipe.view;
 
+import byui.cit260.snipe.control.ChallengeControl;
 import byui.cit260.snipe.enums.ChallengeDescriptionEnum;
+import byui.cit260.snipe.exceptions.ChallengeControlException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import static java.lang.Integer.parseInt;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import snipe.SNIPE;
 
 /**
  *
  * @author Maxwell
  */
-public class ChallengeView extends View{
+public class ChallengeView extends View {
 
     public ChallengeView() {
         super("");
@@ -34,4 +39,31 @@ public class ChallengeView extends View{
         }
         return true;
     }
-}
+
+    public double MathPuzzleTwo() {
+        try {
+            String playerAnswer1 = keyboard.readLine();
+            double playerAnswer1Num = Double.parseDouble(playerAnswer1);
+
+            ChallengeControl cc = new ChallengeControl();
+            double correctAnswer = cc.MathPuzzleTwo(radius, radius2, height1, answer1);
+            //answer is 4825.49
+            if (playerAnswer1Num == correctAnswer) {
+                console.write("A compartment opens and you, exercising the\n"
+                        + "utmost of caution, retrieve the code.\n"
+                        + "the code. Be careful! It's a trap! Well,\n"
+                        + "maybe. It could be perfectly safe too.\n");
+            } else {
+                console.write("Oops! You won't be getting this code you fool!\n"
+                        + "Too bad for you!\n");
+            }
+        } catch(ChallengeControlException cce) {
+            ErrorView.display(ChallengeView.class.getName(), cce.getMessage());
+        } catch(IOException ioe) {
+            ErrorView.display(ChallengeView.class.getName(), ioe.getMessage());
+        }catch(NumberFormatException nfe) {
+            ErrorView.display(ChallengeView.class.getName(), "Aw! Come on! Please\n"+
+                    "enter a positive, rational number, you fool!");
+        }
+        }
+    }
