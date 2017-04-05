76721d00a93f9a72492c44852083541f811ae313
elizabethkirby <elizabethkirby@elizabeths-mbp.fios-router.home>
2017-04-04 23:17:50 -0400

    I think I have SafeHouseView working. STill have to take care of SafeHouseControl and riddleView (or whatever it is...)

--- SNIPE/src/byui/cit260/snipe/view/SafeHouseView.java
+++ SNIPE/src/byui/cit260/snipe/view/SafeHouseView.java
@@ -5,6 +5,9 @@
  */
 package byui.cit260.snipe.view;
 
+import byui.cit260.snipe.exceptions.ChallengeControlException;
+import byui.cit260.snipe.model.Player;
+
 /**
  *
  * @author Maxwell
@@ -12,12 +15,53 @@ package byui.cit260.snipe.view;
 public class SafeHouseView extends View {
 
     public SafeHouseView() {
-        super("You arrive in the safe house*-------------------------*");
+        super("You arrive in the safe houseThere is a pile of large barrels "
+                + "\nis to be found stacked in a shadowy stone recess on the"
+                + "\n right-hand side of the corridor. The barrel two from the "
+                + "\nbottom, middle of the second row, will open if tapped in"
+                + "\nthe rhythm of the 'Mission Impossible' theme.A sloping, "
+                + "\nearthy passage inside the barrel travels upwards a little "
+                + "\nway until a cosy, round, low-ceilinged room is revealed, "
+                + "\nreminiscent of a badger's set. The room is decorated in "
+                + "\nthe cheerful, bee-like colours of yellow and black, e"
+                + "\nmphasised by the use of highly polished, honey-coloured "
+                + "\nwood for the tables and the round doors which lead to the "
+                + "\nmen's and women's dormitories (furnished with comfortable "
+                + "wooden bedsteads, all covered in patchwork quilts). Yes, the"
+                + "\nSafe House is the common room for the House of Hufflepuff."
+                + "\nNow you know you're at Hogwarrs. Welcome Muggle!"
+                + "\nNow, are you feeling rested and healed? Are you ready to"
+                + "\nreturn to the game? Are you reall ready? Are you sure?"
+                + "\nPress 'R' to return to the game.");
+    }
+
+    public static String addHealth(Player player) throws ChallengeControlException {
+        String reply = null;
+        int currentHealth = player.getHealthPoints();
+        if (currentHealth < 100) {
+
+            currentHealth = 100;
+            reply = "You now have " + currentHealth + "points. Go forth and make SNIPE Academy proud";
+        }
+        return reply;
     }
 
     @Override
-    public boolean doAction(String value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean doAction(String choice) {
+        choice = choice.toUpperCase();
+        switch (choice) {
+            case "R":
+                this.returnToMenu();
+                break;
+            default:
+                this.returnToMenu();
+                break;
+        }
+        return false;
     }
 
+    private void returnToMenu() {
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.display();
+    }
 }
